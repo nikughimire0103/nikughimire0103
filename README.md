d6cd6fcc62e1c09230e939fa6ad5b915e0614185
nikughimire0103 <nikughimire@gmail.com>
2025-03-06 16:35:11 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# Personal-Wesbite
+
+This is the personal website of Arsh Dauwa.
+
+
+You can access this site at https://arshdauwa.com/
---
+++ script.js
@@ -0,0 +1,40 @@
+const header = document.querySelector("header");
+
+window.addEventListener ("scroll", function() {
+	header.classList.toggle ("sticky", window.scrollY > 0);
+});
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+	menu.classList.toggle('bx-x');
+	navlist.classList.toggle('active');
+};
+
+window.onscroll = () => {
+	menu.classList.remove('bx-x');
+	navlist.classList.remove('active');
+};
+
+const sr = ScrollReveal ({
+	distance: '45px',
+	duration: 2700,
+	reset: false
+})
+
+sr.reveal('.home-text',{delay:350, origin:'left'})
+sr.reveal('.home-img',{delay:350, origin:'right'})
+
+sr.reveal('.sub-service,.about,.portfolio,.service,.cta,.contact',{delay:200, origin:'bottom'})
+
+function openModal() {
+    document.getElementById("imageModal").style.display = "block";
+    document.getElementById("modalImg").src = document.getElementById("priceImage").src;
+}
+
+function closeModal() {
+    document.getElementById("imageModal").style.display = "none";
+}
+
+
